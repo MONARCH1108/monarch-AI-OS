be20a83a1dc21ed41dad69a6df53bd1606b6efb1
MONARCH1108 <abhayemani8@gmail.com>
2026-03-12 02:06:58 +0530

    Dashboard API Called

--- ClientMN/src/Components/Dashboard/Dashboard.jsx
+++ ClientMN/src/Components/Dashboard/Dashboard.jsx
@@ -1,10 +1,23 @@
+import { useState, useEffect } from "react";
+
 function Dashboard(){
+    const [Hours, setHours] = useState(0)
 
+    const fetchHours = async() =>{
+        const response = await fetch("http://127.0.0.1:8000/analytics/daily")
+        const data = await response.json()
+        const TotalHours = data.reduce((sum, item) => sum + item.hours, 0);
+        setHours(TotalHours.toFixed(2))
+    }
 
+    useEffect(() => {
+        fetchHours();
+    }, []);
+    
     return(
         <div>
             <h2>Dashboard</h2>
-            <p>This is the dashboard page</p>
+            <p>Daily Hours:{Hours}</p>
         </div>
     )
 }
