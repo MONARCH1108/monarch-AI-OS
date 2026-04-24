8780387c946fad035e9e1cb10701c4516940d2f1
MONARCH1108 <abhayemani8@gmail.com>
2026-04-25 00:01:37 +0530

    baseline MonthlyAnalytics

--- Client/src/Components/MonthlyAnalytics/MonthlyAnalytics.jsx
+++ Client/src/Components/MonthlyAnalytics/MonthlyAnalytics.jsx
@@ -1,13 +1,26 @@
+import { Row, Col } from "antd";
+import './MonthlyAnalytics.css'
 import MonthlyStreak from "./MonthlyStreak/MonthlyStreak";
 import DailyAvgHrs from "./DailyAvgHours/DailyAvgHrs";
 
 function MonthlyAnalytics() {
   return (
-    <div>
-        <div>
-            <MonthlyStreak />
-            <DailyAvgHrs />
-        </div>
+    <div style={{ width: "100%" }}>
+
+      <Row gutter={[16, 16]} align="stretch">
+        
+        {/* LEFT - Monthly Streak */}
+        <Col xs={24} md={12} style={{ display: "flex" }}>
+          <MonthlyStreak />
+        </Col>
+
+        {/* RIGHT - Avg Hours */}
+        <Col xs={24} md={12} style={{ display: "flex" }}>
+          <DailyAvgHrs />
+        </Col>
+
+      </Row>
+
     </div>
   );
 }
